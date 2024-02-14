8037e6c7ab3fbdf4fabadd46459edaa846727522
imranmashoud <aaborjona@gmail.com>
2024-02-14 17:49:28 +0600

    Home to playground btn function added

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,17 @@
+// function play() {
+//   //   console.log("Play Start now");
+//   // Step -2: Hide the home screen, to hide home screen add the class hidden to the home section.
+//   const homeSection = document.getElementById("home-screen");
+//   //   console.log(homeSection);
+//   homeSection.classList.add("hidden");
+//   /* ************************************* */
+
+//   // Show Playground section
+//   const playgroundSection = document.getElementById("play-ground");
+//   playgroundSection.classList.remove("hidden");
+// }
+
 function play() {
-  console.log("Play Start now");
+  hideElementById("home-screen");
+  showElementById("play-ground");
 }
